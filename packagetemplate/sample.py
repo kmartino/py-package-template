ccdb137be220450fecfc418ee5e9056073ada878
Karl Martino <karl_martino@comcast.com>
2012-11-14 10:12:18 -0500

    Further refining basic package.

--- packagetemplate/sample.py
+++ packagetemplate/sample.py
@@ -3,3 +3,6 @@ def hello_world():
 
 def print_hello():
     print hello_world()
+
+if __name__ == '__main__':
+    print_hello()
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-setup(
-    name='packagetemplate',
-    version='0.1.0',
-    author='Karl Martino',
-    author_email='kmartino@gmail.com.com',
-    packages=['packatetemplate', 'packagetemplate.test'],
-    #scripts=['if you have a bin dir and scripts that can be called,
-    #specify them here']
-    url='http://pypi.python.org/pypi/TowelStuff/',
-    license='LICENSE.txt',
-    description='A template to use for creating packages.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "nose >= 1.2.1",
-    ],
-)
+config = {
+    'description': 'A template to use for creating packages',
+    'author': 'Karl Martino',
+    'url': 'https://github.com/kmartino/py-package-template',
+    'download_url': 'https://github.com/kmartino/py-package-template',
+    'author_email': 'kmartino@gmail.com',
+    'version': '0.1.0',
+    'install_requires': ['nose'],
+    'packages': ['packagetemplate'],
+    'name': 'packagetemplate',
+    entry_points={
+        'console_scripts': [
+            'pt = packagetemplate.sample:main',
+        ]
+    },
+}
+
+setup(**config)
